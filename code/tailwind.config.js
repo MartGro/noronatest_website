8d8a3307aaadb48009f8952f7fd9c2d2dbb7dab2
Korbinian Poeppel <korbinian.poeppel@gmx.de>
2020-12-10 11:57:03 +0100

    Implement NOrona color code. Update files.

--- code/tailwind.config.js
+++ code/tailwind.config.js
@@ -7,47 +7,28 @@ module.exports = {
   },
   theme: {
     colors: {
-      white: "#88ff88",
-      "text-gray-700": "#88ff88",
-      "bg-gray-800": "#88ff88",
-      "bg-blue-500": "#808080",
+      gray: {
+        light: "#e7e6e6",
+        DEFAULT: "#585858",
+        dark: "#282828",
+      },
+      green: {
+        light: "#c2ff86",
+        DEFAULT: "#8bd839",
+        dark: "#497717",
+      },
+      blue: {
+        dark: "#022a48",
+        DEFAULT: "#0566aff",
+        light: "#269ef9",
+      },
+      red: {
+        dark: "#7a0303",
+        DEFAULT: "#ba0505",
+        light: "#fa4848",
+      },
     },
-    extend: {},
   },
   variants: {},
   plugins: [require("@tailwindcss/typography")],
 };
-// module.exports = {
-//   // future: {
-//   //   purgeLayersByDefault: true,
-//   //   removeDeprecatedGapUtilities: true,
-//   //   defaultLineHeights: true,
-//   //   standardFontWeights: true,
-//   // },
-//   theme: {
-//   colors: {
-//     gray: {
-//       light: '#e7e6e6',
-//       DEFAULT: '#585858',
-//       dark: '#282828',
-//     },
-//     green: {
-//       light: '#c2ff86',
-//       DEFAULT: '#8bd839',
-//       dark: '#497717',
-//     },
-//     blue: {
-//       dark: '#022a48',
-//       DEFAULT: '#0566aff',
-//       light: '#269ef9',
-//     },
-//     red: {
-//       dark: '#7a0303',
-//       DEFAULT: '#ba0505',
-//       light: '#fa4848',
-//     }
-//   }
-// }//,
-//   //variants: {},
-//   //plugins: [require("@tailwindcss/typography")],
-// };
